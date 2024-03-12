bace661e1b60f9d57f19ee4d9abe0ba2e5b57d4a
ChurroDevelop <churroperez25@gmail.com>
2024-03-11 22:55:25 -0500

    Hola

---
+++ assets/js/ejercicio1.js
@@ -0,0 +1,8 @@
+/**
+ * Crea una función para calcular cuantas silabas tiene cada palabra, imprime el resultado
+en la consola.
+ */
+function silabas(palabra) {
+    
+}
+let palabra = prompt("Ingrese la palabra para deerminar las silabas que tiene");
